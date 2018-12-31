baaa8ebc9302f40f25a8f72cd60dc27b95fba990
Pavel Chebotarev <chebotarevp@gmail.com>
2018-12-31 04:26:59 +0300

    Examples: skip messages with null text

--- examples/echobot/source/app.d
+++ examples/echobot/source/app.d
@@ -31,7 +31,7 @@ void listenUpdates()
             logInfo("Got %d updates", updates.length);
 
             foreach (update; updates) {
-                if (!update.message.isNull) {
+                if (!update.message.isNull && !update.message.text.isNull) {
                     logInfo("Text from %s: %s", update.message.chat.id, update.message.text);
                     api.sendMessage(update.message.chat.id, update.message.text);
                 }
