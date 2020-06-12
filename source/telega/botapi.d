97161a4ce7f9f10216ed47f30de8afa0833aae25
Pavel Chebotarev <chebotarevp@gmail.com>
2020-06-12 03:36:14 +0300

    #35 Move payments to a separate module

--- source/telega/botapi.d
+++ source/telega/botapi.d
@@ -10,6 +10,7 @@ import telega.http : HttpClient;
 import telega.serialization : serializeToJsonString, JsonableAlgebraicProxy;
 import telega.telegram.stickers : Sticker;
 import telega.telegram.games : Game, Animation, CallbackGame;
+import telega.telegram.payments : Invoice, SuccessfulPayment, ShippingQuery, PreCheckoutQuery;
 
 enum HTTPMethod
 {
@@ -993,80 +994,6 @@ struct ChosenInlineResult
     Nullable!Location location;
     Nullable!string   inline_message_id;
     string   query;
-}
-
-/*** Payments types ***/
-struct LabeledPrice
-{
-    string label;
-    uint   amount;
-}
-
-struct Invoice
-{
-    string title;
-    string description;
-    string start_parameter;
-    string currency;
-    uint   total_amount;
-}
-
-struct ShippingAddress
-{
-    string country_code;
-    string state;
-    string city;
-    string street_line1;
-    string street_line2;
-    string post_code;
-}
-
-// TODO add nullable fields
-struct OrderInfo
-{
-    string name;
-    string phone_number;
-    string email;
-    Nullable!ShippingAddress shipping_address;
-}
-
-struct ShippingOption
-{
-    string id;
-    string title;
-    LabeledPrice[] prices;
-}
-
-// TODO add nullable fields
-struct SuccessfulPayment
-{
-    string currency;
-    uint   total_amount;
-    string invoice_payload;
-    string shipping_option_id;
-    Nullable!OrderInfo order_info;
-    string telegram_payment_charge_id;
-    string provider_payment_charge_id;
-}
-
-struct ShippingQuery
-{
-    string id;
-    User   from;
-    string invoice_payload;
-    ShippingAddress shipping_address;
-}
-
-// TODO add nullable fields
-struct PreCheckoutQuery
-{
-    string             id;
-    User               from;
-    string             currency;
-    uint               total_amount;
-    string             invoice_payload;
-    string             shipping_option_id;
-    Nullable!OrderInfo order_info;
 }
 
 /*** Telegram Passport ***/
---
+++ source/telega/telegram/payments.d
@@ -0,0 +1,79 @@
+module telega.telegram.payments;
+
+import std.typecons : Nullable;
+import telega.botapi : User;
+
+
+/*** Payments types ***/
+struct LabeledPrice
+{
+    string label;
+    uint   amount;
+}
+
+struct Invoice
+{
+    string title;
+    string description;
+    string start_parameter;
+    string currency;
+    uint   total_amount;
+}
+
+struct ShippingAddress
+{
+    string country_code;
+    string state;
+    string city;
+    string street_line1;
+    string street_line2;
+    string post_code;
+}
+
+// TODO add nullable fields
+struct OrderInfo
+{
+    string name;
+    string phone_number;
+    string email;
+    Nullable!ShippingAddress shipping_address;
+}
+
+struct ShippingOption
+{
+    string id;
+    string title;
+    LabeledPrice[] prices;
+}
+
+// TODO add nullable fields
+struct SuccessfulPayment
+{
+    string currency;
+    uint   total_amount;
+    string invoice_payload;
+    string shipping_option_id;
+    Nullable!OrderInfo order_info;
+    string telegram_payment_charge_id;
+    string provider_payment_charge_id;
+}
+
+struct ShippingQuery
+{
+    string id;
+    User   from;
+    string invoice_payload;
+    ShippingAddress shipping_address;
+}
+
+// TODO add nullable fields
+struct PreCheckoutQuery
+{
+    string             id;
+    User               from;
+    string             currency;
+    uint               total_amount;
+    string             invoice_payload;
+    string             shipping_option_id;
+    Nullable!OrderInfo order_info;
+}
