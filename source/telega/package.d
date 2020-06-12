1219008fa66fdaf255c8f22eb594ebab99ef8451
Pavel Chebotarev <chebotarevp@gmail.com>
2020-06-12 05:58:51 +0300

    #35 added package.d

---
+++ source/telega/package.d
@@ -0,0 +1,12 @@
+module telega;
+
+public import telega.botapi;
+public import telega.helpers;
+public import telega.telegram.basic;
+public import telega.telegram.games;
+public import telega.telegram.inline;
+public import telega.telegram.passport;
+public import telega.telegram.payments;
+public import telega.telegram.stickers;
+public import telega.telegram.updmessages;
+public import telega.telegram.webhook;
