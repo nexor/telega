9a7b37fdca0d85c1887844c3179984b40adb9ba8
Pavel Chebotarev <chebotarevp@gmail.com>
2020-12-25 23:49:56 +0300

    #74 Target dir as CLI argument, code generator WIP

--- generator/source/app.d
+++ generator/source/app.d
@@ -2,16 +2,23 @@ module app;
 
 import std.stdio;
 import std.conv : to;
+import std.string : format;
 import requests : getContent;
 import arsd.dom;
 import generator.parser : TelegramBotApiHTMLParser;
 import generator.generator : CodeGenerator;
+import std.getopt : getopt, defaultGetoptPrinter;
 
-enum PAGE_URL = "https://core.telegram.org/bots/api";
+string url = "https://core.telegram.org/bots/api";
+string targetDir;
 
-int main()
+int main(string[] args)
 {
-    auto document = new Document(PAGE_URL.getContent.to!string);
+    if (processHelp(args)) {
+        return 0;
+    }
+
+    auto document = new Document(url.getContent.to!string);
 
     Element[] h3Items = document.querySelector("div[id=dev_page_content]").querySelectorAll("h3")
         // skipping "Recent changes", "Authorizing your bot", "Making requests", "Local server"
@@ -23,8 +30,38 @@ int main()
         parser.parseSection(el);
     }
 
-    auto generator = new CodeGenerator;
+    const targetDir = getTargetDir();
+    writefln("Generating entities in %s", targetDir);
+    auto generator = new CodeGenerator(targetDir);
     generator.generateFiles(parser.getEntities());
 
     return 0;
+}
+
+private string getTargetDir()
+{
+    import std.file : getcwd, isDir;
+    import std.path : dirSeparator, buildPath;
+
+    string path = buildPath(getcwd(), targetDir);
+    assert(path.isDir, format("Path %s not found or is not a directory"));
+
+    return path;
+}
+
+private bool processHelp(string[] args)
+{
+    auto helpInformation = getopt(
+        args,
+        "target-dir", "Relative path to generate files in", &targetDir,
+        "url", "Telegram bot API documentation URL", &url
+    );
+
+    if (helpInformation.helpWanted) {
+        defaultGetoptPrinter("Telegram bot API entities generator v. 0.1", helpInformation.options);
+
+        return true;
+    }
+
+    return false;
 }
--- generator/source/generator/generator.d
+++ generator/source/generator/generator.d
@@ -1,7 +1,8 @@
 module generator.generator;
 
 import std.stdio : writeln, writefln;
-import generator.parser : TelegramEntity;
+import std.string : format;
+import generator.parser : TelegramEntity, TelegramType, TelegramMethod;
 import std.array : assocArray;
 import std.algorithm.iteration;
 
@@ -11,6 +12,7 @@ shared static this()
 {
     string[] basic = [
         // types
+        "User", "Chat", "Message", "Update",
         // methods
     ];
     string[] groupchat = [
@@ -23,14 +25,74 @@ shared static this()
         // methods
     ];
 
+    basic.each!(val => entityModuleMap[val] = basic.stringof);
     games.each!(val => entityModuleMap[val] = games.stringof);
 }
 
 class CodeGenerator
 {
+    private string modulesDir = "telegram";
+
+    public this(string modulesDir)
+    {
+        this.modulesDir = modulesDir;
+    }
+
     public void generateFiles(TelegramEntity[string] entities)
     {
-        writeln("Generating files ...");
-        writeln(entityModuleMap);
+        foreach (entity; entities) {
+            if (cast(TelegramType)entity) {
+                generateType(cast(TelegramType)entity).writeln();
+            } else if (cast(TelegramMethod)entity) {
+                generateMethod(cast(TelegramMethod)entity).writeln();
+            } else {
+                assert(false, "Unknown entity " ~ entity.name);
+            }
+        }
+        //writeln(entityModuleMap);
     }
+
+    private string generateType(TelegramType entity)
+    {
+        StructItem item = StructItem(entity.name);
+
+        return item.toString();
+    }
+
+    private string generateMethod(TelegramMethod entity)
+    {
+        return "TODO";
+    }
+
+}
+
+struct StructItem
+{
+    private string name;
+    private StructItemField[] fields;
+
+    public this(string name)
+    {
+        this.name = name;
+    }
+
+    public void addField(StructItemField field)
+    {
+        fields ~= field;
+    }
+
+    public string toString()
+    {
+        return format("///\nstruct %s\n{\n%s\n}\n", name, fieldsToString());
+    }
+
+    private string fieldsToString()
+    {
+        return "";
+    }
+}
+
+struct StructItemField
+{
+
 }
